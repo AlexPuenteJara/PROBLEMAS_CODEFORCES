503a610943dcc7f816dac60976c3a5171934137d
AlexPuenteJara <alex.puente1@unmsm.edu.pe>
2021-02-04 09:32:53 -0500

    04/02/2021

--- DIV_2/1478/A. Nezzar and Colorful Balls.cpp
+++ DIV_2/1478/A. Nezzar and Colorful Balls.cpp
@@ -11,15 +11,37 @@ const int inf=1e9;
 vector<int> g[MAXN] ;
 bool used[MAXN] ;
 vector<int> comp ;
+/*Vocabulary:
+keep-mantiene
+*/
 int main(){
  fastio;
  int t;
  cin>>t;
  while(t--){
-	 int n;
-	 cin>>n;
-     cout<<"\n";
+	 int n,a,ca[105];
+	 cin>>n;//
+    for(int i=0;i<=n;i++){
+       ca[i]=0;
+    }
+    for(int i=0;i<n;i++){
+       cin>>a;
+       ca[a]++;  
+    }
+    int mx=0;
+    for(int i=0;i<=n;i++){
+       mx=max(mx,ca[i]);
+    }  
+    cout<<mx<<"\n";
     }
    return 0;
 }
- 
+/*RESUMEN DEL PROBLEM:
+La idea de este problema es colorear las bolas de varios colores, de tal manera que
+los números coloreados del mismo color sea una secuencia estrictamente creciente.
+
+IDEAS QUE RESUELVEN EL PROBLEMA
+Se demuestra que la máxima cantidad de números iguales es la cantidad de colores que
+se debe usar.
+
+*/ 
